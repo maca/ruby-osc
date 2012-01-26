502e301bd510d51730e6bb7bf36bcf7e37383d4f
Florian Eitel <feitel@indeedgeek.de>
2012-01-26 19:20:06 +0100

    some performance improvements for stream scanner

--- lib/ruby-osc/streamscanner.rb
+++ lib/ruby-osc/streamscanner.rb
@@ -5,13 +5,15 @@ module OSC
     def initialize
       @scanner = StringScanner.new ""
     end
+
     def << arg
       @scanner << arg
     end
+
     def tryparse
       m = Message.decode_message @scanner
-      puts m.inspect
-      @scanner = StringScanner.new @scanner.rest
+      @scanner.string = @scanner.rest
+      return m
     end
   end
 end
