15aee4d6e1213bf724acca2a12062dd7890e309b
Florian Eitel <feitel@indeedgeek.de>
2012-01-27 22:36:32 +0100

    added benchmark for streamscan

---
+++ streamscanner_benchmark.rb
@@ -0,0 +1,37 @@
+require 'benchmark'
+
+$:.unshift( File.join( File.dirname( __FILE__), 'lib' ) )
+
+require 'ruby-osc/streamscanner.rb'
+
+@ss = OSC::StreamScanner.new
+
+count = 10_000
+
+Benchmark.bm(23) do |x|
+  x.report("add new string") do
+    count.times do
+      @ss << "/bar/foo\000\000\000\000,iiii\000\000\000\000\000\000\004\000\000\000\003\000\000\000\002\000\000\000\001".force_encoding("binary")
+    end
+  end
+
+  x.report("parse string") do
+    count.times do
+      @ss.tryparse
+    end
+  end
+
+
+  x.report("try parse each message") do
+    count.times do
+      OSC::Message.decode("/bar/foo\000\000\000\000,iiii\000\000\000\000\000\000\004\000\000\000\003\000\000\000\002\000\000\000\001".force_encoding("binary"))
+    end
+  end
+
+  x.report("parse failure") do
+    count.times do
+      @ss.tryparse rescue nil
+    end
+  end
+end
+
